22495a0dccfcd9bc5154a939ed17e547a29ac80e
eros francesco reale <eros.francesco.francesco@gmail.com>
2024-09-15 16:57:53 +0200

    Update tests.js

--- pins/tests.js
+++ pins/tests.js
@@ -1,24 +1,14 @@
-const rpio = require('rpio');
-
-
-const pin = 12;
-const motor = rpio.open(pin, rpio.PWM);
-rpio.pwmSetClockDivider(128);
-rpio.pwmSetRange(pin, 8500);
-
-rpio.pwmSetData(pin, 1000);
-
-/*
 const { Gpio } = require('pigpio');
 const { openPin, readPin, writeToPin, writePWMToPin } = require('./pins');
-/** */
 
 const tests = async () => {
-    /*
-    const pin = new Gpio(18);
-    // openPin('18');
-    console.log(pin.getMode())
+    const pin = openPin('18');
 
+    const value = await readPin(pin);
+    console.log('Pin read:', value);
+    
+    /*
+    // SERVO
     let pulseWidth = 500;
     let increment = 100;
 
