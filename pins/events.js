f73ce81c1b35f72c2ef619d37ae2f753858c33e5
erosfrancesco <eros.francesco.francesco@gmail.com>
2024-10-01 13:20:31 +0200

    events names

--- pins/events.js
+++ pins/events.js
@@ -5,34 +5,6 @@ const events = {
     WRITE: (pin) => 'pin.write.' + pin,
     PWM: (pin) => 'pin.pwm.' + pin,
     SERVO: (pin) => 'pin.servo.' + pin, // TODO: - Implement
-
-
-    /*
-    PIN_OPEN: {
-        EVENT: () => 'pin-open',
-        SUCCESS: (pin) => 'pin-success-open-' + pin,
-    },
-
-    PIN_READ: {
-        EVENT: (pin) => 'pin-read-' + pin,
-        SUCCESS: (pin) => 'pin-success-read-' + pin,
-    },
-
-    PIN_WRITE: {
-        EVENT: (pin) => 'pin-write-' + pin,
-        SUCCESS: (pin) => 'pin-success-write-' + pin,
-    },
-
-    PIN_PWM: {
-        EVENT: (pin) => 'pin-pwm-' + pin,
-        SUCCESS: (pin) => 'pin-success-pwm-' + pin,
-    },
-
-    SERVO_WRITE: {
-        EVENT: (pin) => 'pin-servo-' + pin,
-        SUCCESS: (pin) => 'pin-success-servo-' + pin,
-    }
-    /** */
 };
 
 module.exports = events;
